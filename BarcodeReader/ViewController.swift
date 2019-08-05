30a98a1a3b6cae456f5f652bcdab5a987f590d73
橋本 龍 <hashimotoryoh@gmail.com>
2019-08-05 15:28:51 +0900

    create new single view application

---
+++ BarcodeReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeReader
+//
+//  Created by 橋本 龍 on 2019/08/05.
+//  Copyright © 2019 Ryoh Hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
